e4c693b52851bdf67786872a86db16e24d2ee448
Hatim Errimy <hatim-s.o.s@outlook.fr>
2022-04-08 17:20:03 +0000

    Patient Controller + patients.html

--- src/main/java/jeetp/patientsmvc/repositories/PatientRepository.java
+++ src/main/java/jeetp/patientsmvc/repositories/PatientRepository.java
@@ -6,3 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
 }
+
--- src/main/java/jeetp/patientsmvc/web/PatientController.java
+++ src/main/java/jeetp/patientsmvc/web/PatientController.java
@@ -1,13 +1,22 @@
 package jeetp.patientsmvc.web;
 
+import jeetp.patientsmvc.entities.Patient;
 import jeetp.patientsmvc.repositories.PatientRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
+@AllArgsConstructor
 public class PatientController {
     private PatientRepository patientRepository;
-
-    public PatientController(PatientRepository patientRepository) {
-        this.patientRepository = patientRepository;
+    @GetMapping(path = "/index")
+    public String patients(Model model){
+        List<Patient> patients= patientRepository.findAll();
+        model.addAttribute("listPatients",patients);
+        return "patients";
     }
 }
