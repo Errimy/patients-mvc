9d2240aa85e3b6619a18955af11872b888e138d2
Hatim Errimy <hatim-s.o.s@outlook.fr>
2022-04-09 21:24:24 +0000

    Search and Delete done.

--- src/main/java/jeetp/patientsmvc/repositories/PatientRepository.java
+++ src/main/java/jeetp/patientsmvc/repositories/PatientRepository.java
@@ -1,9 +1,12 @@
 package jeetp.patientsmvc.repositories;
 
 import jeetp.patientsmvc.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Page<Patient> findByNomContains(String kw, Pageable pageable);
 
 }
 
--- src/main/java/jeetp/patientsmvc/web/PatientController.java
+++ src/main/java/jeetp/patientsmvc/web/PatientController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -19,11 +20,28 @@ public class PatientController {
     @GetMapping(path = "/index")
     public String patients(Model model,
                            @RequestParam(name = "page", defaultValue = "0") int page,
-                           @RequestParam(name = "size", defaultValue = "5") int size){
-        Page<Patient> pagePatients= patientRepository.findAll(PageRequest.of(page, size));
+                           @RequestParam(name = "size", defaultValue = "5") int size,
+                           @RequestParam(name = "keyword", defaultValue = "") String keyword){
+        Page<Patient> pagePatients= patientRepository.findByNomContains(keyword,PageRequest.of(page, size));
         model.addAttribute("listPatients",pagePatients.getContent());
         model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
         model.addAttribute("currentPage", page);
+        model.addAttribute("keyword",keyword);
         return "patients";
+    }
+
+    @GetMapping("/delete")
+    public String delete (Long id, String keyword, int page){
+        patientRepository.deleteById(id);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+    @GetMapping("/")
+    public String home(){
+        return "redirect:/index";
+    }
+    @GetMapping("/patients")
+    @ResponseBody
+    public List<Patient> listPatients(){
+        return patientRepository.findAll();
     }
 }
