90ed9339f27519c3fdfa7452ee8c3473bb4dd135
Hatim Errimy <hatim-s.o.s@outlook.fr>
2022-04-21 23:05:40 +0000

    Form to add patients with spring validator

--- src/main/java/jeetp/patientsmvc/PatientsMvcApplication.java
+++ src/main/java/jeetp/patientsmvc/PatientsMvcApplication.java
@@ -15,13 +15,13 @@ public class PatientsMvcApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PatientsMvcApplication.class, args);
 	}
-	@Bean
+	//@Bean
 	CommandLineRunner commandLineRunner(PatientRepository patientRepository){
 		return args -> {
-			/*patientRepository.save(new Patient(null,"Hassan",new Date(), false, 12));
-			patientRepository.save(new Patient(null,"Hatim",new Date(), true, 64));
-			patientRepository.save(new Patient(null,"Hafid",new Date(), false, 50));
-			patientRepository.save(new Patient(null,"Aymane",new Date(), true, 10));*/
+			patientRepository.save(new Patient(null,"Hassan",new Date(), false, 112));
+			patientRepository.save(new Patient(null,"Hatim",new Date(), true, 164));
+			patientRepository.save(new Patient(null,"Hafid",new Date(), false, 150));
+			patientRepository.save(new Patient(null,"Aymane",new Date(), true, 110));
 
 			patientRepository.findAll().forEach(p -> {
 				System.out.println(p.getNom());
--- src/main/java/jeetp/patientsmvc/web/PatientController.java
+++ src/main/java/jeetp/patientsmvc/web/PatientController.java
@@ -7,10 +7,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -43,5 +46,18 @@ public class PatientController {
     @ResponseBody
     public List<Patient> listPatients(){
         return patientRepository.findAll();
+    }
+
+    @GetMapping("/formPatients")
+    public String formPatients(Model model){
+        model.addAttribute("patient", new Patient());
+        return "formPatients";
+    }
+
+    @PostMapping("/save")
+    public String save(Model model, @Valid Patient patient, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) return "formPatients";
+        patientRepository.save(patient);
+        return "formPatients";
     }
 }
