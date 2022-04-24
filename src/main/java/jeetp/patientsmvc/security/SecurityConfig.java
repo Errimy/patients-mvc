ae67a0592b10a4228f266da9ab4e2d55b23efd68
Hatim Errimy <hatim-s.o.s@outlook.fr>
2022-04-24 14:57:13 +0000

    spring security

---
+++ src/main/java/jeetp/patientsmvc/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package jeetp.patientsmvc.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder=passwordEncoder();
+        String encodedPWD=passwordEncoder.encode("1234");
+        System.out.println(encodedPWD);
+        auth.inMemoryAuthentication()
+                .withUser("user1").password(encodedPWD).roles("USER")
+                .and()
+                .withUser("user2").password(passwordEncoder.encode("1111")).roles("USER")
+                .and()
+                .withUser("admin").password(passwordEncoder.encode("2345")).roles("USER","ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin();
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder (){
+        return new BCryptPasswordEncoder();
+    }
+}
