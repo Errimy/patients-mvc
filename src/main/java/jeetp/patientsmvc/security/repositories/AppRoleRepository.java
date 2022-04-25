35314b19bf592a107b1f1160631c681ca0b9d6ce
Hatim Errimy <hatim-s.o.s@outlook.fr>
2022-04-25 00:44:04 +0000

    Switched to UserDetails Auth.

---
+++ src/main/java/jeetp/patientsmvc/security/repositories/AppRoleRepository.java
@@ -0,0 +1,8 @@
+package jeetp.patientsmvc.security.repositories;
+
+import jeetp.patientsmvc.security.entities.AppRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppRoleRepository extends JpaRepository<AppRole, Long> {
+    AppRole findByRoleName(String roleName);
+}
